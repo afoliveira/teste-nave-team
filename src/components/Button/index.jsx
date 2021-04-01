8601b9614887384b2e530a0f297f47830b21cabf
Afonso <afonso.noliveira29@gmail.com>
2021-03-31 23:53:20 -0300

    Create Button Component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Btn, Container} from './styles';
+
+const Button = ({children, width, padding, onClick, customStyle={}}) => {
+  return (
+    <Container style={{justifyContent: 'flex-end'}}>
+      <Btn onClick={onClick} btnWidth={width} btnPadding={padding}>
+        {children}
+      </Btn>
+    </Container>
+  )
+}
+
+export default Button;
---
+++ src/components/Button/styles.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const Btn = styled.button`
+  border: ${props => props.btnBorder || 'none'};
+  background-color: #212121;
+  color: #fff;
+  width: ${props => props.btnWidth || '176px'};
+  height: 40px;
+  padding: 8px ${props => props.btnPadding || '16.7px'}
+`;
