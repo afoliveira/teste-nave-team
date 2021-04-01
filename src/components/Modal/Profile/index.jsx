168107d5c3c30ed7e29834cd05fbbcedffb3b512
Afonso <afonso.noliveira29@gmail.com>
2021-03-31 23:56:11 -0300

    Create modal for display the naver profile

---
+++ src/components/Modal/Profile/index.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+import trash from '../../../assets/icons/trash.svg'
+import pencil from '../../../assets/icons/pencil.svg'
+import close from '../../../assets/icons/close.svg'
+
+import {handleDeleteNaver} from '../../../services/requests'
+
+import {Container, ContainerModal, Content, Actions, InfoContent, CloseButton} from './styles'
+
+const ModalProfile = ({data, handleClose, show}) => {
+
+  const getAge = (age) => {
+    const currentDate = new Date();
+    const pastDate = new Date(age);
+    let diff = currentDate.getFullYear() - pastDate.getFullYear();
+    if(diff > 0) {
+      return `${diff} anos`;
+    } else {
+      diff = (currentDate.getFullYear() - pastDate.getFullYear()) * 12
+      return `${diff} meses`;
+    }
+  }
+
+  const handleDelete = async (id) => {
+    handleDeleteNaver(id).then(() => {
+      handleClose();
+      window.location.reload();
+    })
+  }
+
+  return (
+    <Container show={show}>
+      <ContainerModal>
+        <div>
+          <img width='503px' height='503px' src={data.url} alt={data.name} />
+        </div>
+        <Content>
+          <CloseButton onClick={handleClose}>
+            <img src={close} alt='close'></img>
+          </CloseButton>
+          <InfoContent>
+            <div className='container-content'>
+              <h2>{data.name}</h2>
+              <span>{data.job_role}</span>
+            </div>
+            <div className='container-content'>
+              <h3>Idade</h3>
+              <span>{getAge(data.birthdate)}</span>
+            </div>
+            <div className='container-content'>
+              <h3>Tempo de empresa</h3>
+              <span>{getAge(data.admission_date)}</span>
+            </div>
+            <div className='container-content'>
+              <h3>Projetos que participou</h3>
+              <span>{data.project}</span>
+            </div>
+          </InfoContent>
+          <Actions>
+            <div style={{cursor: 'pointer'}} onClick={() => handleDelete(data.id)}>
+              <img src={trash} alt='trash'></img>
+            </div>
+            <div style={{cursor: 'pointer'}}>
+              <img src={pencil} alt='pencil'></img>
+            </div>
+          </Actions>
+        </Content>
+      </ContainerModal>
+    </Container>
+  )
+}
+
+export default ModalProfile;
---
+++ src/components/Modal/Profile/styles.jsx
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+  display: ${({show}) => show ? 'block' : 'none'};
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0,0,0, 0.7);
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
+
+
+export const ContainerModal = styled.div`
+  display: flex;
+  position: fixed;
+  margin: auto;
+  z-index: 1001;
+  background-color: #fff;
+  width: 1006px;
+  height: 503px;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+export const InfoContent = styled.div`
+  align-content: space-between;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 32px;
+  
+  .container-content {
+    margin-bottom: 24px;
+    
+    h2 {
+      font-size: 1.5rem;
+      line-height: 36px;
+      font-weight: 600;
+    }
+    
+    h2, h3 {
+      margin-bottom: 10px;
+    }
+    
+    h3 {
+      font-size: 1rem;
+      line-height: 24px;
+      font-weight: 600;
+    }
+    
+    span {
+      font-size: 1rem;
+      line-height: 24px;
+    }
+  }
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: space-between;  
+  padding: 30.2px;
+`;
+
+export const CloseButton = styled.div`
+  align-self: flex-end;
+  width: 24px;
+  height: 24px;
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 60px;
+`;
