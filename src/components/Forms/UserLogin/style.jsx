aba6db00768e27a9e0b0173ae3378fe0987ec893
Afonso <afonso.noliveira29@gmail.com>
2021-03-31 23:55:19 -0300

    Create userlogin form

---
+++ src/components/Forms/UserLogin/style.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  border: 2px solid #000;
+  padding: 32px;
+  margin: auto;
+  position: absolute;
+  top: 0; bottom: 0;
+  left: 0; right: 0;
+  height: 410px;
+  width: 450px;
+`;
+
